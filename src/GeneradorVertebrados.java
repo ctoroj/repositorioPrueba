21366468942c94cbcde14aa663783ec5d179e165
ctoroj <ctoroj@uninorte.edu.co>
2022-09-06 21:16:45 -0500

    Delete tiburon and don't know what more

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -3,8 +3,9 @@ public class GeneradorVertebrados {
         Vertebrado[] miVertebrados = new Vertebrado[4];
         miVertebrados[0] = new Vertebrado("Camello", "Mamifero"); 
         miVertebrados[1] = new Vertebrado("Vaca", "Mamifero");
-        miVertebrados[2] = new Vertebrado("Tiburón", "Pez"); 
         
+        //Elimine al tiburon
+        //Ahora no sé que más poner.
         return miVertebrados;
     }
 }
